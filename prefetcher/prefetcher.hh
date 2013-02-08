a392f7e4c91fba5404c531df419703756ff35570
Benjamin Bjørnseth <benjamin@bjornseth.net>
2013-02-08 15:12:16 +0100

    Added prefetcher interface which all our prefetchers are supposed to implement

---
+++ prefetcher/prefetcher.hh
@@ -0,0 +1,29 @@
+#ifndef _PREFETCHER_H_
+#define _PREFETCHER_H_
+
+struct PrefetchDecision
+{
+  explicit PrefetchDecision()
+      : wantToPrefetch(false),
+        prefetchAddress(0x0) { /*empty */}
+
+  explicit PrefetchDecision(Addr addr)
+      : wantToPrefetch(true),
+        prefetchAddress(addr) { /* empty */ }
+
+  const bool wantToPrefetch;
+  Addr prefetchAddress;
+};
+
+class Prefetcher
+{
+ public:
+  virtual ~Prefetcher() {};
+  unsigned int prefetch_attempts() const = 0;
+  unsigned int prefetch_hits() const = 0;
+  void increase_aggressiveness() = 0;
+  void decrease_aggressiveness() = 0;
+  PrefetchDecision get_prefetch_address(AccessStat stat) = 0;
+};
+
+#endif /* _PREFETCHER_H_ */
