82d4e72737ee79d53e43eddc6df66127b59d7f65
Benjamin Bjørnseth <benjamin@bjornseth.net>
2013-02-13 22:14:04 +0100

    Remove even more const code.

--- prefetcher/ghb_pcdc.hh
+++ prefetcher/ghb_pcdc.hh
@@ -7,51 +7,51 @@
 
 struct TableEntry
 {
-  explicit TableEntry() : address(0x0), previousMiss(0) { /* empty */ }
-  explicit TableEntry(Addr addr, TableEntry *prevMiss)
-      : address(addr), previousMiss(prevMiss) { /*empty */}
-  const Addr address;
-  TableEntry *previousMiss;
- private:
-  //Disallow copying..?
-  TableEntry& operator=(const TableEntry&);
-  TableEntry(const TableEntry&);
+    explicit TableEntry() : address(0x0), previousMiss(0) { /* empty */ }
+    explicit TableEntry(Addr addr, TableEntry *prevMiss)
+        : address(addr), previousMiss(prevMiss) { /*empty */}
+    const Addr address;
+    TableEntry *previousMiss;
+// private:
+//     //Disallow copying..?
+//     TableEntry& operator=(const TableEntry&);
+//     TableEntry(const TableEntry&);
 
 };
 
 template <unsigned int TableSize>
 class GlobalHistoryBuffer
 {
- public:
-  explicit GlobalHistoryBuffer()
-      : head_(0), evictingOldEntry_(false) { /*empty */}
-  TableEntry* insert(const AccessStat &stat, TableEntry *previousMiss);
- private:
-  TableEntry* findFirstEntryReferencing(const TableEntry * const e) const;
-  std::size_t head_;
-  bool evictingOldEntry_;
-  TableEntry buffer_[TableSize];
+public:
+    explicit GlobalHistoryBuffer()
+        : head_(0), evictingOldEntry_(false) { /*empty */}
+    TableEntry* insert(const AccessStat &stat, TableEntry *previousMiss);
+private:
+    TableEntry* findFirstEntryReferencing(const TableEntry * const e);
+    std::size_t head_;
+    bool evictingOldEntry_;
+    TableEntry buffer_[TableSize];
   
 };
 
 template<unsigned int TableSize>
-TableEntry* GlobalHistoryBuffer<TableSize>::findFirstEntryReferencing(const TableEntry *const e) const
+TableEntry* GlobalHistoryBuffer<TableSize>::findFirstEntryReferencing(const TableEntry *const e)
 {
-  for (int i = head_; i >= 0; i--)
-  {
-    if (buffer_[i].previousMiss == e)
+    for (int i = head_; i >= 0; i--)
     {
-      return &buffer_[i];
+        if (buffer_[i].previousMiss == e)
+        {
+            return &buffer_[i];
+        }
     }
-  }
-  for (int i = TableSize - 1; i > head_; i--)
-  {
-    if (buffer_[i].previousMiss == e)
+    for (int i = TableSize - 1; i > head_; i--)
     {
-      return &buffer_[i];
+        if (buffer_[i].previousMiss == e)
+        {
+            return &buffer_[i];
+        }
     }
-  }
-  return 0;
+    return 0;
 }
 
 
@@ -60,109 +60,109 @@ TableEntry* GlobalHistoryBuffer<TableSize>::insert(
     const AccessStat &stat,
     TableEntry *previousMiss)
 {
-  if (evictingOldEntry_)
-  {
-    TableEntry *e = findFirstEntryReferencing(&buffer_[head_]);
-    if (e != 0)
+    if (evictingOldEntry_)
     {
-      e->previousMiss = 0;
+        TableEntry *e = findFirstEntryReferencing(&buffer_[head_]);
+        if (e != 0)
+        {
+            e->previousMiss = 0;
+        }
+        //If we are kicking out the previous miss, do not link to it...
+        if (&buffer_[head_] == previousMiss)
+        {
+            previousMiss = 0;
+        }
     }
-    //If we are kicking out the previous miss, do not link to it...
-    if (&buffer_[head_] == previousMiss)
+    buffer_[head_] = TableEntry(stat.mem_addr, previousMiss);
+    TableEntry *newEntry = &buffer_[head_];
+    head_++;
+    if (head_ >= TableSize)
     {
-      previousMiss = 0;
+        head_ = 0;
+        evictingOldEntry_ = true;
     }
-  }
-  buffer_[head_] = TableEntry(stat.mem_addr, previousMiss);
-  TableEntry *newEntry = &buffer_[head_];
-  head_++;
-  if (head_ >= TableSize)
-  {
-    head_ = 0;
-    evictingOldEntry_ = true;
-  }
-  return newEntry;
+    return newEntry;
 }
 
 template <unsigned int TableSize>
 class IndexTable
 {
- public:
-  explicit IndexTable()
-      : head_(0)
-  {
-    memset(buffer_, 0, sizeof(buffer_));
-  }
+public:
+    explicit IndexTable()
+        : head_(0)
+        {
+            memset(buffer_, 0, sizeof(buffer_));
+        }
 
-  //Overload [] instead? :x
-  TableEntry* previousAccessTo(Addr pc) const;
-  void setPreviousAccessTo(Addr pc, TableEntry *e);
- private:
-  TableEntry buffer_[TableSize];
-  std::size_t head_;
+    //Overload [] instead? :x
+    TableEntry* previousAccessTo(Addr pc) const;
+    void setPreviousAccessTo(Addr pc, TableEntry *e);
+private:
+    TableEntry buffer_[TableSize];
+    std::size_t head_;
 };
 
 template<unsigned int TableSize>
 TableEntry* IndexTable<TableSize>::previousAccessTo(Addr pc) const
 {
-  std::size_t index = pc % TableSize;
-  if (buffer_[index].address == pc)
-  {
-    return buffer_[index].previousMiss;
-  }
-  return 0;
+    std::size_t index = pc % TableSize;
+    if (buffer_[index].address == pc)
+    {
+        return buffer_[index].previousMiss;
+    }
+    return 0;
 }
 
 template<unsigned int TableSize>
 void IndexTable<TableSize>::setPreviousAccessTo(Addr pc, TableEntry *e)
 {
-  std::size_t index = pc % TableSize;
-  buffer_[index] = TableEntry(pc, e);
+    std::size_t index = pc % TableSize;
+    buffer_[index] = TableEntry(pc, e);
 }
 
 
 template <unsigned int TableSize>
 class GHB_PCDC : public Prefetcher
 {
- public:
-  explicit GHB_PCDC()
-      : attempts_(0),
-        hits_(0),
-        numBlocksToPrefetch_(2),
-        historyBuffer_(),
-        indexTable_() { /*empty */}
-  unsigned int prefetch_attempts() const { return attempts_; }
-  unsigned int prefetch_hits() const { return hits_; }
-  void increase_aggressiveness() {
-    if (numBlocksToPrefetch_ > 1)
-      numBlocksToPrefetch_--;
-  }
-  void decrease_aggressiveness() {
-    numBlocksToPrefetch_++;
-  }
-  PrefetchDecision react_to_access(AccessStat stat);
- private:
-  void insert(const AccessStat &stat);
+public:
+    explicit GHB_PCDC()
+        : attempts_(0),
+          hits_(0),
+          numBlocksToPrefetch_(2),
+          historyBuffer_(),
+          indexTable_() { /*empty */}
+    unsigned int prefetch_attempts() const { return attempts_; }
+    unsigned int prefetch_hits() const { return hits_; }
+    void increase_aggressiveness() {
+        if (numBlocksToPrefetch_ > 1)
+            numBlocksToPrefetch_--;
+    }
+    void decrease_aggressiveness() {
+        numBlocksToPrefetch_++;
+    }
+    PrefetchDecision react_to_access(AccessStat stat);
+private:
+    void insert(const AccessStat &stat);
   
-  void compute_delta_table(
-      const AccessStat &stat,
-      std::vector<int> &deltaTable) const;
+    void compute_delta_table(
+        const AccessStat &stat,
+        std::vector<int> &deltaTable) const;
 
-  int pastPreviousOccurrenceOfLastPair(const std::vector<int> &deltas) const;
+    int pastPreviousOccurrenceOfLastPair(const std::vector<int> &deltas) const;
 
-  unsigned int attempts_;
-  unsigned int hits_;
-  unsigned int numBlocksToPrefetch_;
-  GlobalHistoryBuffer<TableSize> historyBuffer_;
-  IndexTable<TableSize> indexTable_;
+    unsigned int attempts_;
+    unsigned int hits_;
+    unsigned int numBlocksToPrefetch_;
+    GlobalHistoryBuffer<TableSize> historyBuffer_;
+    IndexTable<TableSize> indexTable_;
 };
 
 template<unsigned int TableSize>
 void GHB_PCDC<TableSize>::insert(const AccessStat &stat)
 {
-  TableEntry *lastEntry = indexTable_.previousAccessTo(stat.pc);
-  TableEntry *newEntry = historyBuffer_.insert(stat, lastEntry);
-  indexTable_.setPreviousAccessTo(stat.pc, newEntry);
+    TableEntry *lastEntry = indexTable_.previousAccessTo(stat.pc);
+    TableEntry *newEntry = historyBuffer_.insert(stat, lastEntry);
+    indexTable_.setPreviousAccessTo(stat.pc, newEntry);
 }
 
 
@@ -171,62 +171,50 @@ void GHB_PCDC<TableSize>::compute_delta_table(
     const AccessStat &stat,
     std::vector<int> &deltaTable) const
 {
-  for (TableEntry *e = indexTable_.previousAccessTo(stat.pc);
-       e->previousMiss != 0; e = e->previousMiss)
-  {
-    deltaTable.push_back(e->address - e->previousMiss->address);
-  }
+    for (TableEntry *e = indexTable_.previousAccessTo(stat.pc);
+         e->previousMiss != 0; e = e->previousMiss)
+    {
+        deltaTable.push_back(e->address - e->previousMiss->address);
+    }
 }
 
 template<unsigned int TableSize>
 int GHB_PCDC<TableSize>::pastPreviousOccurrenceOfLastPair(
     const std::vector<int> &deltas) const
 {
-  if (deltas.size() < 2) return -1;
+    if (deltas.size() < 2) return -1;
 
-  int d1 = deltas[deltas.size() - 2], d2 = deltas[deltas.size() - 1];
-  for (int i = deltas.size() - 4; i >= 0; i--)
-  {
-    if (deltas[i] == d1 && deltas[i+1] == d2)
-      return i + 2;
-  }
-  return -1;
+    int d1 = deltas[deltas.size() - 2], d2 = deltas[deltas.size() - 1];
+    for (int i = deltas.size() - 4; i >= 0; i--)
+    {
+        if (deltas[i] == d1 && deltas[i+1] == d2)
+            return i + 2;
+    }
+    return -1;
 }
 
 
 template<unsigned int TableSize>
 PrefetchDecision GHB_PCDC<TableSize>::react_to_access(AccessStat stat)
 {
-  insert(stat);
-  std::vector<int> deltas;
-  compute_delta_table(stat, deltas);
-  int index = pastPreviousOccurrenceOfLastPair(deltas);
-  if (index == -1)
-  {
-    return PrefetchDecision(); //Do not prefetch
-  }
-  else
-  {
-    std::vector<Addr> addrs;
-    for (int i = index; i < index + numBlocksToPrefetch_; ++i)
+    insert(stat);
+    std::vector<int> deltas;
+    compute_delta_table(stat, deltas);
+    int index = pastPreviousOccurrenceOfLastPair(deltas);
+    if (index == -1)
     {
-      addrs.push_back(stat.mem_addr + deltas[i]);
+        return PrefetchDecision(); //Do not prefetch
     }
-    return PrefetchDecision(addrs);
-  }
+    else
+    {
+        std::vector<Addr> addrs;
+        for (int i = index; i < index + numBlocksToPrefetch_; ++i)
+        {
+            addrs.push_back(stat.mem_addr + deltas[i]);
+        }
+        return PrefetchDecision(addrs);
+    }
 }
-
-// template<unsigned int TableSize>
-// PrefetchDecision GHB_PCDC::react_to_access(AccessStat stat)
-// {
-//   insert(stat);
-//   deltas = computeDeltaTable();
-//   lastPair = getLastPair(deltas, stat);
-//   index = searchForPreviousOccurrenceOf(lastPair);
-//   return PrefetchDecision(
-//       (for i <- 0 until numBlocksToPrefetch)
-//       yield stat.addr + deltas[index + i];);
-// }
 
 #endif /* _GHB_PCDC_H_ */
 
