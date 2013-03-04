62e5c257b08fc411a7c4eae406cb1dab6797d7e3
Håkon Furre Amundsen <hakonfam@L430.(none)>
2013-03-04 16:46:39 +0100

    MERK: run.sh er skummel nå, sjekk den.

--- prefetcher/stride_stack.hh
+++ prefetcher/stride_stack.hh
@@ -1,13 +1,13 @@
 #ifndef _STRIDE_STACK_H_
 #define _STRIDE_STACK_H_
-
+#include "prefetcher.hh"	
 class StrideStack
 {
  private:
   unsigned int currentSize, index;
   unsigned int stack[5];
  public:
-  void push(int value);
+  void push(Addr value);
 	void printStack();
   unsigned int getCurrentSize();
 	unsigned int getItemAt(int index);
